38458891dbbcc62e9fcc279498dfe1808bf1ff31
Shyam Sulbhewar <56698436+shyamwin@users.noreply.github.com>
2020-08-21 22:05:27 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,7 +1,7 @@
 import  smtplib # module to send mail
 sender ="adsj9050@gmail.com"  #gives your or sender mail
 receiver= "shyamsulbhewar007@gmail.com"	#gives receiver or authority mail
-password= "Aaditya@123"   #gives password of sender mail
+password= "*****************"   #gives password of sender mail
 message= "Sorry to disturb your webserver not working properly, please do check" #message you want to send
 server=smtplib.SMTP("smtp.gmail.com",587)  #use to congiure mail server
 server.starttls() # to start server
